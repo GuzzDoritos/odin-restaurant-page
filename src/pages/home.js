7ecd7906caa3f42683d2ccd19ca01fdd1bb8233c
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-11 18:43:35 -0300

    Change image loading order and change float

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,14 +7,15 @@ export default function renderHome() {
     //create DOM elements
     const elements = {
         homeHeader: document.createElement("h1"),
+        homeImage: new Image(250),
         homeParagraph: document.createElement("p"),
-        homeImage: new Image(200),
     }
     
     //Add content to elements
     elements.homeHeader.textContent = "A Restaurant";
     elements.homeParagraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut placerat ipsum, ut semper orci. Nunc maximus ac odio ut mattis. Nam pharetra ante commodo dui pretium vulputate. Sed sit amet tortor leo. Nulla condimentum nulla non mi vestibulum accumsan. Vivamus fringilla convallis velit, eget congue mi iaculis eu. Vivamus consectetur ultricies consequat. Phasellus et placerat dui."
     elements.homeImage.src = kirbyBurg;
+    elements.homeImage.style.float = "right";
 
     //Add classes
 
