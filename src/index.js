4a8165318a3421fad0b0db47ad18bdd827c5ebaf
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-12 17:24:54 -0300

    Add about button event listener

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./styles.css";
-import { renderHome, renderMenu } from "./pageBarrel.js";
+import { renderHome, renderMenu, renderAbout } from "./pageBarrel.js";
 
 (function Page() {
     // cache DOM
     const contentDiv = document.querySelector("#content");
     const homeBtn = document.querySelector("button:nth-child(1)");
     const menuBtn = document.querySelector("button:nth-child(2)");
+    const aboutBtn = document.querySelector("button:nth-child(3)");
 
     homeBtn.addEventListener("click", () => {
         contentDiv.replaceChildren();
@@ -15,6 +16,11 @@ import { renderHome, renderMenu } from "./pageBarrel.js";
     menuBtn.addEventListener("click", () => {
         contentDiv.replaceChildren();
         renderMenu();
+    })
+
+    aboutBtn.addEventListener("click", () => {
+        contentDiv.replaceChildren();
+        renderAbout();
     })
 
     window.addEventListener("load", renderHome);    
