80e3b7fcc164f5831a95c168d2bbf230528a3a05
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-12 17:27:41 -0300

    Add image to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import kirbyBurg from "./img/kirbyburg.png"
+import kirbyChef from "./img/kirbychef.png"
 
 export function renderAbout() {
     //Query the content div
@@ -7,15 +7,15 @@ export function renderAbout() {
     //create DOM elements
     const elements = {
         aboutHeader: document.createElement("h1"),
-        // homeImage: new Image(250),
+        aboutImage: new Image(250),
         aboutParagraph: document.createElement("p"),
     }
     
     //Add content to elements
     elements.aboutHeader.textContent = "About";
     elements.aboutParagraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut placerat ipsum, ut semper orci. Nunc maximus ac odio ut mattis. Nam pharetra ante commodo dui pretium vulputate. Sed sit amet tortor leo. Nulla condimentum nulla non mi vestibulum accumsan. Vivamus fringilla convallis velit, eget congue mi iaculis eu. Vivamus consectetur ultricies consequat. Phasellus et placerat dui."
-    // elements.homeImage.src = kirbyBurg;
-    // elements.homeImage.style.float = "right";
+    elements.aboutImage.src = kirbyChef;
+    elements.aboutImage.style.float = "right";
 
     //Add classes
 
