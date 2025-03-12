9e31106777ea1903a1fd3039d76515fed3026d33
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-12 14:14:33 -0300

    Add renderMenu module to imports and create event listener for the menu button in the nav

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import "./styles.css";
-import renderHome from "./pageBarrel.js";
+import { renderHome, renderMenu } from "./pageBarrel.js";
 
 (function Page() {
     // cache DOM
     const contentDiv = document.querySelector("#content");
     const homeBtn = document.querySelector("button:nth-child(1)");
+    const menuBtn = document.querySelector("button:nth-child(2)");
 
     homeBtn.addEventListener("click", () => {
         contentDiv.replaceChildren();
         renderHome();
+    })
+
+    menuBtn.addEventListener("click", () => {
+        contentDiv.replaceChildren();
+        renderMenu();
     })
 
     window.addEventListener("load", renderHome);    
